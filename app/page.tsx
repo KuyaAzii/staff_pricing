b0aa46566cdd51f46e12f6f95292d5da8191a366
Ace Polero <43778193+KuyaAzii@users.noreply.github.com>
2024-02-23 12:53:31 +0800

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 // pages/index.tsx
 
+import dynamic from 'next/dynamic';
+
+
+import '../app/globals.css';
+
+const Login = dynamic(() => import('../components/Login'), { ssr: false });
 
 export default function Home() {
   return (
 
       <div>
-        
+     <Login/> 
       </div>
 
   );
